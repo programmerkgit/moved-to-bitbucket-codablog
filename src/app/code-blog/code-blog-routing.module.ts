8165469c0cd8bd21f242653ca265b8d1bfaecb96
admin <k.kubo.private.mobile@gmail.com>
2019-06-13 08:30:19 +0900

    update ng

--- src/app/code-blog/code-blog-routing.module.ts
+++ src/app/code-blog/code-blog-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { CodeBlogComponent } from './code-blog.component';
 import { IndexComponent } from './index/index.component';
 import { GitComponent } from './git/git.component';
+import { TestComponent } from '../test/test.component';
 
 const routes: Routes = [
   {
     path: '', component: CodeBlogComponent, children: [
       {path: '', component: IndexComponent},
       {path: 'git', component: GitComponent},
-      {path: 'bitcoin', loadChildren: '../bitcoin/bitcoin.module#BitcoinModule'}
+      {path: 'bitcoin', loadChildren: () => import('../bitcoin/bitcoin.module').then(m => m.BitcoinModule)},
+      {path: 'test', component: TestComponent}
     ]
   }
 ];
--- src/app/test/ng-custom-element/ng-custom-element.component.ts
+++ src/app/test/ng-custom-element/ng-custom-element.component.ts
@@ -1,16 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnDestroy, OnInit } from '@angular/core';
+import { createCustomElement, NgElement, WithProperties } from '@angular/elements';
+import { HostComponent } from '../host/host.component';
 
 @Component({
   selector: 'app-ng-custom-element',
   templateUrl: './ng-custom-element.component.html',
   styleUrls: [ './ng-custom-element.component.scss' ]
 })
-export class NgCustomElementComponent implements OnInit {
+export class NgCustomElementComponent implements OnInit, OnDestroy {
 
-  constructor() {
+  element;
+
+  constructor(
+    private  injector: Injector
+  ) {
+    /*
+     The @angular/elements package exports a createCustomElement() API that provides a bridge
+     from Angular's component interface and change detection functionality to
+     the built-in DOM API.
+    */
+    const hostElement = createCustomElement(HostComponent, {injector});
+    if (document.createElement('new-host').constructor === HTMLElement) {
+      customElements.define('new-host', hostElement);
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.element) {
+      // document.body.removeChild(this.element);
+      this.element.parentNode.removeChild(this.element);
+    }
   }
 
   ngOnInit() {
+    const element: NgElement & WithProperties<HostComponent>
+      = document.createElement('new-host') as any;
+    this.element = element;
+    document.body.appendChild(element);
   }
 
 }
